830c1a1dd602d05603cb551898405c7114e83a7a
TheConqueror327 <mark.erdohelyi@gmail.com>
2026-04-18 23:04:19 +0200

    Add simple testing for the new kinematic_chain and Fk methods

--- LeRobot_SO101/LeRobot_SO101.py
+++ LeRobot_SO101/LeRobot_SO101.py
@@ -1,4 +1,17 @@
+import sys
+import numpy as np
+
+sys.path.append('../kinematics_project')
+
 from kinematics.robot_nDoF import Robot
 
-#SO101 = Robot.from_urdf('LeRobot_SO101.urdf')
+SO101 = Robot.from_urdf('LeRobot_SO101/LeRobot_SO101.urdf')
 
+
+for i in SO101.kinematic_chain:
+    print(i)
+
+
+T_end = SO101.FK(np.array([0, 0, 0, 0, 0, 0]))
+
+print(T_end)
